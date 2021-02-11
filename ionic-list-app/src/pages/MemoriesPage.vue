3970b1b255f4b2a5acbc251d856de52911154f0a
principalkelvoe <you@example.com>
2021-02-11 23:08:13 +0300

    memories page built added essential components

--- ionic-list-app/src/pages/MemoriesPage.vue
+++ ionic-list-app/src/pages/MemoriesPage.vue
@@ -1,8 +1,23 @@
-<template>
+<template> 
+    <ion-page>
+        <ion-header>
+            <ion-title>All Memories</ion-title>
+        </ion-header>
+        <ion-content>
+            <h2>These are my memories - list to be added</h2>
+        </ion-content>
+    <ion-page>
     
 </template>
 <script>
+import { IonPage, IonHeader, IonTitle, IonContent} from '@ionic/vue';
+
 export default {
-    
+    components:{
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonContent
+    }
 }
 </script>
